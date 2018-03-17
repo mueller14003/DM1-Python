dc179936de17cc1f85554f8030216a28e2030393
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-03-16 23:57:50 -0600

    Update on 03/16/18 at 23:57:50

--- Huffman_Tree_Maker.py
+++ Huffman_Tree_Maker.py
@@ -1,21 +1,19 @@
 input_text = input("Please enter desired text: ")
 
-mktree = lambda n,i,r,p='': {'0':mktree(n,n[i][0],r,p+'0'),'1':mktree(n,n[i][1],r,p+'1')} if len(n[i]) == 2 else setr(n,i,r,p)
+mktree = lambda n,i,r,p='': {'0':mktree(n,n[i][0],r,p+'0'),'1':mktree(n,n[i][1],r,p+'1')} if len(n[i]) == 2 else setr(i,r,p)
 
-def setr(n,i,r,p=''):
+def setr(i,r,p=''):
     r[i] = p
     return i
 
 def Huffman(_vals):
-    vals,a1,a2 = _vals.copy(),0,0
+    vals,a1,a2,d = _vals.copy(),0,0,{}
     nodes = {n:[] for n in vals.keys()}
     while len(vals) > 1:
         s_vals = sorted(vals.items(),key=lambda x:x[1])
         a1,a2 = s_vals[0][0],s_vals[1][0]
         vals[a1+a2],nodes[a1+a2] = vals.pop(a1)+vals.pop(a2),[a1,a2]
-    d,root = {},a1+a2
-    tree = mktree(nodes,root,d)
-    return d,tree
+    return d,mktree(nodes,a1+a2,d)
 
 to_list = lambda s: list(map(lambda x: x,s))
 to_string = lambda l: ''.join(l)
@@ -23,20 +21,16 @@ get_weight = lambda m: list(map(lambda i: (to_list(m).count(i),i),sorted(list(di
 d,tree = Huffman({l:v for (v,l) in get_weight(input_text)})
 
 def H_decode(d,message):
-    decoded,id,m,current = [],dict(zip(d.values(),d.keys())),to_list(message),[]
+    de,id,m,c = [],dict(zip(d.values(),d.keys())),to_list(message),[]
     for i in m:
-        current.append(i)
-        if to_string(current) in d.values():
-            decoded.append(id[to_string(current)])
-            current = []
-    return to_string(decoded)
+        c.append(i)
+        if to_string(c) in d.values():
+            de.append(id[to_string(c)])
+            c = []
+    return to_string(de)
 
 H_encode = lambda d,m: to_string(list(map(lambda i: d[i],m)))
 
-print("Dictionary:",d)
-print("Tree:",tree)
 encoded = H_encode(d,input_text)
-print("Encoded:",encoded)
-print("Encoded Hex:",hex(int(encoded,2)))
 decoded = H_decode(d,encoded)
-print("Decoded:",decoded)
+print("Dictionary: {}\nTree: {}\nEncoded: {}\nEncoded Hex: {}\nDecoded: {}".format(d,tree,encoded,hex(int(encoded,2)),decoded))
