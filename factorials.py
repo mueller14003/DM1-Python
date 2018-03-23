007fd307d64180190c466da882d601b93c23786d
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-03-23 01:41:00 -0600

    Update on 03/23/18 at 01:41:00

--- factorials.py
+++ factorials.py
@@ -1,11 +1,10 @@
-import sys
 from functools import *
 from operator import mul
 
 rfact = lambda n: n*rfact(n-1) if n>1 else n
 mfact = lambda n: reduce(mul,range(1,n+1),1)
 
-f = mfact(1000000)
+f = mfact(10000)
 flen = len(str(f))
 print(f)
 print(flen)
