04e2058e992fefcce9a6615bf467b5c6166dfb56
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-03-28 19:24:38 -0600

    Update on 03/28/18 at 19:24:38

--- car_cdr.py
+++ car_cdr.py
@@ -1,6 +1,12 @@
+def isiterable(n):
+    try: _ = [x for x in n]
+    except TypeError: return False
+    else: return True
+
+
 class elisp:
     def __init__(self):
-        self.destroy_nest = lambda n: self.destroy_nest(n[0]) if self.isiterable(n) and len(n) == 1 else n
+        self.destroy_nest = lambda n: self.destroy_nest(n[0]) if isiterable(n) and len(n) == 1 else n
         self.new_func = lambda func: self.make_new(None, func[1:len(func) - 1]) if self.valid(func) and func[0] == 'c' and func[len(func) - 1] == 'r' else lambda n: n
         self.cr = {'a':self.a,'d':self.d}
         self.new_f = lambda new,old: lambda *n: new(old(*n))
@@ -9,15 +15,15 @@ class elisp:
     def d(self,n,*a):
         n = self.destroy_nest(n)
         if a: return list(a)
-        if not self.isiterable(n): return n
+        if not isiterable(n): return n
         if len(n) == 1: return n[0]
-        if self.isiterable(n[1:]) and len(n[1:]) == 1: return n[1:][0]
+        if isiterable(n[1:]) and len(n[1:]) == 1: return n[1:][0]
         else: return list(n[1:])
 
     def a(self,n,*a):
         n = self.destroy_nest(n)
         if a: return n
-        if self.isiterable(n): return n[0]
+        if isiterable(n): return n[0]
         else: return n
 
     def call(self,func,*n):
@@ -30,15 +36,10 @@ class elisp:
         if len(l) == 1:
             if f: return self.new_f(self.cr[l[0]],f)
             else: return self.cr[l[0]]
-        else:
-            if f: return self.make_new(self.new_f(self.cr[l[len(l)-1]],f),l[0:len(l)-1])
-            else: return self.make_new(self.cr[l[len(l)-1]],l[0:len(l)-1])
+        if f: return self.make_new(self.new_f(self.cr[l[len(l)-1]],f),l[0:len(l)-1])
+        else: return self.make_new(self.cr[l[len(l)-1]],l[0:len(l)-1])
 
-    def isiterable(self,n):
-        try: _ = [x for x in n]
-        except TypeError: return False
-        else: return True
 
-print(elisp().call('cddr',[[1,2],[3,4],[6,5]]))
-car = elisp().new_func('car')
-print(car([[1,2],[3,4],[6,5]]))
+# print(elisp().call('cddr',[[1,2],[3,4],[6,5]]))
+# car = elisp().new_func('car')
+# print(car([[1,2],[3,4],[6,5]]))
---
+++ pylisp.py
@@ -0,0 +1,3 @@
+from car_cdr import elisp
+car = elisp().new_func('car')
+print(car([[1,2],3,4,5]))
