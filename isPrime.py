e9e1b5fba71cf2b1a4a215f0b52173c049f16987
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-04-04 15:24:34 -0600

    Update on 04/04/18 at 15:24:34

--- isPrime.py
+++ isPrime.py
@@ -4,17 +4,18 @@
 #
 # isPrime = lambda n: n in p_list
 #
-# def is_prime(n):
-#     for i in range(2, int(n**.5) + 1):
-#         if n % i == 0: return False
-#     return True
-#
-# #Is_Prime = lambda n: False if n%i==0 for i in range(2,int(n**5)+1) else True
-# def nothing(): pass
-# is_Prime = lambda n: False if False in list(map(lambda i: n%i,range(2,int(n**.5)+1))) else True
-# prime_list = lambda n: list(filter(lambda i: i if is_prime(i) else nothing(),list(range(2,n+1))))
-#
-# print(is_prime(123456789101112131415246801))
+def is_prime(n):
+    for i in range(2, int(n**.5) + 1):
+        if n % i == 0: return False
+    return True
 
-#[print(n)for n in range(2,100)]
-[print(n)for n in range(2,100)if all(n%i for i in range(2,n))]
+#Is_Prime = lambda n: False if n%i==0 for i in range(2,int(n**5)+1) else True
+def nothing(): pass
+is_Prime = lambda n: False if False in list(map(lambda i: n%i,range(2,int(n**.5)+1))) else True
+prime_list = lambda n: list(filter(lambda i: i if is_prime(i) else nothing(),list(range(2,n+1))))
+printer = lambda n,f: [*map(print,f(n))]
+
+#printer(100,prime_list)
+
+[print(n)for n in range(2,99)if all(n%i for i in range(2,n))]
+#[*map(print,list(iter(input,'.')))] #This is cool!! Though not related to prime numbers...
--- pi.py
+++ pi.py
@@ -1,3 +1,4 @@
 fact = lambda n: n*fact(n-1) if n > 1 else 1
 calc_pi = lambda n: (12/(640320*(640320**.5)))*sum(list(map(lambda k: (((-1)**k)*fact(6*k)*(13591409+(545140134*k)))/(fact(3*k)*(fact(k)**3)*640320**(3*k)),range(0,n+1))))
-print(1/calc_pi(1))
+print(format(1/calc_pi(1),'.100f'))
+
