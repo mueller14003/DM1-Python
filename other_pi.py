3992616d49e3fb608d388a6fccf7c98c65053664
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-04-05 13:40:00 -0600

    Update on 04/05/18 at 13:40:00

--- other_pi.py
+++ other_pi.py
@@ -1,4 +1,4 @@
 from decimal import*
 getcontext().prec=1001
 D=Decimal
-print(sum(1/D(16)**k*(D(4)/(8*k+1)-D(2)/(8*k+4)-D(1)/(8*k+5)-D(1)/(8*k+6))for k in range(999)))
+print(sum(D(16)**-k*(D(4)/(8*k+1)-D(2)/(8*k+4)-D(1)/(8*k+5)-D(1)/(8*k+6))for k in range(999)))
