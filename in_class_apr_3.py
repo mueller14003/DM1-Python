d57e32134ab4180549dd750a9d5d5ff6191f1608
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-04-05 02:31:31 -0600

    Update on 04/05/18 at 02:31:30

---
+++ in_class_apr_3.py
@@ -0,0 +1,23 @@
+# t=lambda x:sum([(-1)**n*x**(2*n-1)/(2*n-1)for n in range(1,10000)])
+# p=4*(4*t(1/5)-t(1/239))
+# print(p)
+
+# def p():
+#     q=t=k=1
+#     m=x=3
+#     r=0
+#     for j in range(4320):
+#         if 4*q+r-t<m*t:
+#             yield m
+#             q,r,t,k,m,x=10*q,10*(r-m*t),t,k,(10*(3*q+r))//t-10*m,x
+#         else:q,r,t,k,m,x=q*k,(2*q+r)*x,t*x,k+1,(q*(7*k+2)+r*x)//(t*x),x+2
+# print("".join(['3.']+[str(i)for i in p()][1:]))
+
+def p():
+  q,r,t,k,m,x=1,0,1,1,3,3
+  for j in range(4320):
+    if 4*q+r-t<m*t:
+      yield m
+      q,r,t,k,m,x=10*q,10*(r-m*t),t,k,10*(3*q+r)//t-10*m,x
+    else:q,r,t,k,m,x=q*k,(2*q+r)*x,t*x,k+1,(q*(7*k+2)+r*x)//(t*x),x+2
+print('3.'+"".join([*map(str,p())][1:]))
