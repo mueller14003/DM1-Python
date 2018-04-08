353f49f934e48b54c7756c291e1022340c51878e
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-04-08 00:20:44 -0600

    Update on 04/08/18 at 00:20:44

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
-a=[0,1]
-for _ in[1]*29:a+=[sum(a[-2:])]
-[*map(print,a)]
-
+# a=[0,1]
+# for _ in[1]*29:a+=[sum(a[-2:])]
+# [*map(print,a)]
+a=[0,1];exec('print(a[-2]);a+=[sum(a[-2:])];'*31)
 # a,b=0,1
 # for i in[1]*29:
 #  c=a+b
@@ -11,3 +11,4 @@ for _ in[1]*29:a+=[sum(a[-2:])]
 
 # f=lambda n:n if n<2else f(n-1)+f(n-2)
 # [print(f(i))for i in range(31)]
+#s='s={0!r};print(s.format(s))';print(s.format(s))
--- pi.py
+++ pi.py
@@ -3,27 +3,31 @@
 # PI
 # from decimal import*
 # getcontext().prec=1001
-# D=Decimal
-# print(sum(D(16)^-k*(D(4)/(8*k+1)-D(2)/(8*k+4)-D(1)/(8*k+5)-D(1)/(8*k+6))for k in range(999)))
+# D=Decimal;o=D(1)
+# print(sum(D(16)**-n*(4*o/(8*n+1)-o/(4*n+2)-o/(8*n+5)-o/(8*n+6))for n in range(999)))
 
 # EULER'S NUMBER
 # import math,decimal
 # decimal.getcontext().prec=1002
 # print(str(sum(decimal.Decimal(1)/math.factorial(k)for k in range(999)))[:-1])
 
+# BETTER EULER'S NUMBER
+# from decimal import*
+# getcontext().prec=1001
+# print(Decimal(1).exp())
+
 # PHI
 # from decimal import*
 # getcontext().prec=1001
 # print((1+Decimal(5).sqrt())/2)
 
-#n=0;exec('print(n);n=n+2^-(n^n+2)%3;'*25)
+# EVIL NUMBERS
+# n=0;exec('print(n);n=n+2^-(n^n+2)%3;'*25)
 
-# for n in range(800):
-#  if~bin(n).count('1')%2:print(n)
-#[print(n)for n in range(800)if~bin(n).count('1')%2]
-for i in range(1,101):
- n=''
- if not i%3:n+='Fizz'
- if not i%5:n+='Buzz'
- if n:print(n)
- else:print(i)
+
+# for i in range(1,101):
+#  n=''
+#  if not i%3:n+='Fizz'
+#  if not i%5:n+='Buzz'
+#  if n:print(n)
+#  else:print(i)
