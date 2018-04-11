e2eac63ac9336a899f5836b9b0049a29ff563b66
mueller14003 <34406644+mueller14003@users.noreply.github.com>
2018-04-10 18:30:45 -0600

    Update on 04/10/18 at 18:30:45

--- factorials.py
+++ factorials.py
@@ -4,14 +4,14 @@ import sys
 sys.setrecursionlimit(10000)
 import time
 
-r=lambda n:n*r(n-1)if n>1else n
-f=lambda n:0**n or n*f(n-1)
-mfact = lambda n: reduce(mul,range(1,n+1),1)
-
-t1 = time.time()
-f1 = mfact(10000)
-t2 = time.time()
-print("Value: {}\nDigits: {}\nTime: {}".format(f1,len(str(f1)),t2-t1))
+# r=lambda n:n*r(n-1)if n>1else n
+# f=lambda n:0**n or n*f(n-1)
+# mfact = lambda n: reduce(mul,range(1,n+1),1)
+#
+# t1 = time.time()
+# f1 = mfact(10000)
+# t2 = time.time()
+# print("Value: {}\nDigits: {}\nTime: {}".format(f1,len(str(f1)),t2-t1))
 
 make_f=lambda f:(lambda n:reduce(mul,range(n%len(f)or len(f),n+1,len(f)),1))if all(x=='!'for x in f)else lambda n:n
 call_f=lambda n,f:make_f(f)(n)
@@ -22,4 +22,5 @@ df = make_f('!!')
 tf = make_f('!!!')
 qf=make_f('!!!!')
 
-print(df(7))
+
+print(sum(-df(2*n-1)/sf(2*n+3)for n in range(0,100)))
